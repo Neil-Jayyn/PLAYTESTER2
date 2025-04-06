38c733c53e85ebbfc76741920a6741f2fdcd667f
Moyo Dawodu <moyodawodu@gmail.com>
2025-04-06 17:37:31 -0600

    Before Adding cutscene changes to build

---
+++ PLAYTESTER (not a scam)/Assets/ChangeScenes.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ChangeScenes : MonoBehaviour
+{
+    public float changeTime;
+    public string mainScene;
+    public string sceneName2;
+
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //hardcoded to time duration of second clip, since this script will only be used for the cutscene
+
+        changeTime -= Time.deltaTime;
+        if (changeTime <= 2.85 && changeTime > 0)
+        {
+            SceneManager.LoadScene(sceneName2);
+        }
+
+        else if (changeTime <= 0)
+        {
+            SceneManager.LoadScene(mainScene);
+        }
+    }
+}
